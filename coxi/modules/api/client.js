b7fb5c98f27731798b4168559476bdd68c4be50f
SiGae <sigae@protonmail.com>
2020-08-18 17:25:18 +0900

    delete legacy and add gitignore

--- coxi/modules/api/client.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const serverPath = "";
-const client = axios.create();
-
-export default client;
--- coxi/modules/api/loginResister.js
+++
@@ -1,9 +0,0 @@
-import client, { serverPath } from "./client";
-
-export const loginInfo = ({ id, pw }) => {
-  return client.post(serverPath + "/login", { id, pw });
-};
-
-export const resisterInfo = ({ id, pw1, pw2, email }) => {
-  return client.post(serverPath + "/resiter", { id, pw1, pw2, email });
-};
